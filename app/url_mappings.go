6aed9cf06a7c46226722181242e494eeabbaafc0
JErBerlin <jeronirm@gmail.com>
2020-01-01 20:35:31 +0100

    controllers refactoring 3 (cumbersome git integration with goland)

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,13 +1,14 @@
 package app
 
 import (
+	"github.com/JErBerlin/bookstore_users-api/controllers/defaultResponse"
 	"github.com/JErBerlin/bookstore_users-api/controllers/ping"
 	"github.com/JErBerlin/bookstore_users-api/controllers/users"
 )
 
 func mapURLs() {
 	router.GET("/ping", ping.ping_controller)
-	router.GET("/", DefaultResponse.default_controller)
+	router.GET("/", defaultResponse.default_controller)
 
 	router.GET("/users/:user_id", users.GetUser)
 	router.POST("/users", users.CreateUser)
