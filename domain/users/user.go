3969ebcc4e4d0a628db9eec0d88e5b518ad80495
JErBerlin <jeronirm@gmail.com>
2020-01-02 20:10:37 +0100

    implementing POST user, unmarshalling 5

--- domain/users/user.go
+++ domain/users/user.go
@@ -1,9 +1,13 @@
 package users
 
 type User struct {
-	Id          int64
-	FirstName   string
-	LastName    string
-	Email       string
-	DateCreated string
+	Id          int64  `json: "id"`
+	FirstName   string `json: "first_name"`
+	LastName    string `json: "last_name"`
+	Email       string `json: "email"`
+	DateCreated string `json: "date_created"`
+}
+
+func NewUser(id int64, firstName string, lastName string, email string, dateCreated string) *User {
+	return &User{Id: id, FirstName: firstName, LastName: lastName, Email: email, DateCreated: dateCreated}
 }
